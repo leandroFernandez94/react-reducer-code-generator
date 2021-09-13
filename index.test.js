00543538962fc0d76c2261d38a4cf058b8909f1b
Leandro Fernandez <leandroofernandezz@gmail.com>
2021-09-13 18:04:53 -0300

    add tests and babel

---
+++ index.test.js
@@ -0,0 +1,75 @@
+import { beforeEach } from "@jest/globals";
+import RRCG from "./index";
+
+describe("when state is an empty object", () => {
+  const state = {};
+  it("should only return SET_STATE action", () => {
+    const [actions] = RRCG(state);
+    expect(actions).toHaveLength(1);
+    expect(actions[0]).toBe("SET_STATE");
+  });
+});
+
+describe("when state is a primitive value", () => {
+  let state = "test";
+  it("should only return SET_STATE action", () => {
+    let result = RRCG(state);
+    let actions = result[0];
+    expect(actions).toHaveLength(1);
+    expect(actions[0]).toBe("SET_STATE");
+
+    state = 4;
+    result = RRCG(state);
+    actions = result[0];
+    expect(actions).toHaveLength(1);
+    expect(actions[0]).toBe("SET_STATE");
+
+    state = true;
+    result = RRCG(state);
+    actions = result[0];
+    expect(actions).toHaveLength(1);
+    expect(actions[0]).toBe("SET_STATE");
+  });
+});
+
+describe("when initial state is an object with one level deep properties", () => {
+  const initialState = {
+    a: 1,
+    b: 1,
+  };
+  let [actions, reducer] = RRCG(initialState);
+
+  it("should return actions for properties", () => {
+    expect(actions).toHaveLength(2);
+    expect(actions).toContain("SET_A");
+    expect(actions).toContain("SET_B");
+  });
+
+  describe("and reducer is used on a state object to modify one property", () => {
+    const stateSnapshot = {
+      a: 2,
+      b: 4,
+    };
+    const result = reducer(stateSnapshot, { type: "SET_A", payload: 45 });
+
+    it("should return a new snapshot with that property modified", () => {
+      expect(Object.keys(result)).toHaveLength(2);
+      expect(result.a).toEqual(45);
+      expect(result.b).toEqual(stateSnapshot.b);
+    });
+  });
+
+  describe("and reducer is used on an inexistent property", () => {
+    const stateSnapshot = {
+      a: 2,
+      b: 4,
+    };
+    const result = reducer(stateSnapshot, { type: "SET_C", payload: 45 });
+
+    it("shouldn't modify the snapshot", () => {
+      expect(Object.keys(result)).toHaveLength(2);
+      expect(result.a).toEqual(stateSnapshot.a);
+      expect(result.b).toEqual(stateSnapshot.b);
+    });
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,70 @@
+function mapPropNameToActionName(propName, value, prefix = "") {
+  const upperVersion = propName.replace(/([A-Z])/g, "_$1").toUpperCase();
+  const actionName = `${prefix ? prefix : ""}${upperVersion}`;
+
+  const propnameSetter = `SET_${actionName}`;
+
+  if (typeof value !== "object") {
+    return [propnameSetter];
+  }
+
+  const childPropNames = Object.keys(value);
+  return [
+    propnameSetter,
+    childPropNames.flatMap((childProp) =>
+      mapPropNameToActionName(childProp, value[childProp], `${actionName}__`)
+    ),
+  ].flat();
+}
+
+function generateActions(state) {
+  // only one action if state is a number/string/boolean/array
+  if (typeof state !== "object") {
+    return ["SET_STATE"];
+  }
+
+  const propsNames = Object.keys(state);
+
+  if (!propsNames.length) return ["SET_STATE"];
+  return propsNames.flatMap((propName) =>
+    mapPropNameToActionName(propName, state[propName])
+  );
+}
+
+function getPathFromActionString(actionString) {
+  const path = actionString.toLowerCase().split("__");
+  path[0] = path[0].split("set_")[1];
+  return path.map((pathStep) =>
+    pathStep.replace(/_(.{1})/g, (_, letter) => letter.toUpperCase())
+  );
+}
+
+function generateReducerFn(actions) {
+  const reducers = actions.reduce((acum, actionString) => {
+    const path = getPathFromActionString(actionString);
+    const actionReducer = (state, payload) => {
+      path.reduce((acc, key, i) => {
+        if (acc[key] === undefined) acc[key] = {};
+        if (i === path.length - 1) acc[key] = payload;
+        return acc[key];
+      }, state);
+      return state;
+    };
+
+    acum[actionString] = actionReducer;
+    return acum;
+  }, {});
+
+  return (state, { type, payload }) => {
+    return reducers[type] ? reducers[type](state, payload) : state;
+  };
+}
+
+function reactReducerCodeGenerator(initialState) {
+  const actions = generateActions(initialState);
+  const reducerFn = generateReducerFn(actions);
+
+  return [actions, reducerFn];
+}
+
+module.exports = reactReducerCodeGenerator;
